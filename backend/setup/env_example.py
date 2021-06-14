5a695ab0d9bf73d7706be1b9b9bbc475f98c9da0
André Arruda <andre23arruda@gmail.com>
2021-06-14 00:12:18 -0300

    improve backend

--- backend/setup/env_example.py
+++ backend/setup/env_example.py
@@ -17,6 +17,7 @@ def get_allowed_hosts():
 os.environ['SECRET_KEY'] = '(_nz-7j=$7we1y6$8i%bftxt2l_udez1ou_x7@*_x8t+(0e2lk'
 os.environ['DEBUG'] = 'true'
 os.environ['ALLOWED_HOSTS'] = json.dumps(get_allowed_hosts())
+os.environ['BASE_URL'] = f'http://{ get_ip_address() }:8000'
 os.environ['LANGUAGE_CODE'] = 'pt-br'
 os.environ['TIME_ZONE'] = 'America/Sao_Paulo'
 os.environ['AUTHOR'] = 'andre23arruda'
